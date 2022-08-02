c7a5b82127c2271b040a183f5981ba4c76ebfa0b
nyan-ko <dumbstupidcatboy@gmail.com>
2022-08-02 18:31:36 -0400

    Move sketchpad creation into function
    Also adds event listener skeleton to sketchpad squares

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const sketchpad = document.querySelector(".sketchpad");
-console.log(sketchpad);
+function createSquares(size) {
+    size = size ?? 16;
 
-const size = 16;
-
-for (let i = 0; i < size; i++) {
-    for (let j = 0; j < size; j++) {
+    const sketchpad = document.querySelector(".sketchpad");
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement("div");
+        row.classList.add("sketchpad-row");
         
+        for (let j = 0; j < size; j++) {
+            const square = document.createElement("div");
+            square.classList.add("sketchpad-square");
+            square.addEventListener('click', onClick);
+            row.appendChild(square);
+        }
+
+    sketchpad.appendChild(row);
     }
 }
+
+function onClick(e) {
+    this.style.backgroundColor = 'black';
+}
+
+createSquares(16);
