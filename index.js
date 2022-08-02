38be93f27419ecb171a362fde014ca639c6fc9d7
nyan-ko <dumbstupidcatboy@gmail.com>
2022-08-01 21:42:04 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const sketchpad = document.querySelector(".sketchpad");
+console.log(sketchpad);
+
+const size = 16;
+
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+        
+    }
+}
